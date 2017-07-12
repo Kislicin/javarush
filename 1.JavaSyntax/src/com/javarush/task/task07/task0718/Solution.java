144b383661985453303162fbaa2b89c7fc79e0e9
Kislicin <kislicinpp@mail.ru>
2017-07-12 09:40:01 +0300

    added task 0716

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0718/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task07.task0718;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Проверка на упорядоченность
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> list = new ArrayList<String>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for(int i=0; i<10; i++){
+            list.add(reader.readLine());
+        }
+        int min = list.get(0).length();
+        for(int i = 0; i < list.size(); i++){
+            if(min>list.get(i).length()){
+                System.out.println(i);
+                break;
+            }
+            else min = list.get(i).length();
+        }
+    }
+}
+
