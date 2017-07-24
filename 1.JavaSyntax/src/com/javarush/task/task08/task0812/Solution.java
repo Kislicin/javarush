91627ea91afd9cd4af86dd9fc4d26e0d646a10ad
Kislicin <kislicinpp@mail.ru>
2017-07-24 10:10:12 +0300

    added last task from lesson 8 lect 6

--- 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
@@ -9,18 +9,24 @@ Cамая длинная последовательность
 public class Solution {
     public static void main(String[] args) throws IOException {
         ArrayList<Integer> list = new ArrayList<>();
-        ArrayList<Integer> maxList = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int maxPos = 1;
+        int maxPosPred = 1;
         for (int i = 0; i<10; i++){
             list.add(Integer.parseInt(reader.readLine()));
         }
         for (int i = 0; i<list.size(); i++){
             if ((i+1) != list.size()) {
-                if (list.get(i) == list.get(i + 1))
+                if (list.get(i) == list.get(i + 1)) {
                     maxPos++;
+                    if (maxPosPred<maxPos){
+                        maxPosPred=maxPos;
+                    }
+                }
+                else
+                    maxPos = 1;
             }
         }
-        System.out.println(maxPos);
+        System.out.println(maxPosPred);
     }
 }
