f677277d424d96c81a11edadf8d03a9ac4f5bf6c
Kislicin <kislicinpp@mail.ru>
2017-07-27 15:07:41 +0300

    added all tasks from lesson 8 lect 8

--- 1.JavaSyntax/src/com/javarush/task/task08/task0817/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0817/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task08.task0817;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /* 
@@ -9,13 +11,31 @@ import java.util.Map;
 
 public class Solution {
     public static HashMap<String, String> createMap() {
-        //напишите тут ваш код
-
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Киркоров", "Филипп");
+        map.put("Пугачева", "Алла");
+        map.put("Тюрин", "Михаил");
+        map.put("Аршавин", "Михаил");
+        map.put("Щелкова", "Валерия");
+        map.put("Агутин", "Федор");
+        map.put("Пупков", "Иван");
+        map.put("Пригожина", "Валерия");
+        map.put("Иванов", "Петр");
+        map.put("Сергеев", "Федор");
+        return map;
     }
 
     public static void removeTheFirstNameDuplicates(HashMap<String, String> map) {
-        //напишите тут ваш код
-
+        ArrayList<String> values = new ArrayList<String>(map.values());
+        for (String s1 : values){
+            int count = 0;
+                for (String s2 : values ){
+                    if (s1.equals(s2))
+                        count++;
+                    if (count>=2)
+                        removeItemFromMapByValue(map, s1);
+                }
+        }
     }
 
     public static void removeItemFromMapByValue(HashMap<String, String> map, String value) {
